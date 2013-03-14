938a9743cda85c382e10c80f1d5c04d9f3be500b
Lewis Taylor <lewis@dangerfarms.com>
2013-03-14 22:56:34 +0000

    database dir changed

--- src/project/settings/_common.py
+++ src/project/settings/_common.py
@@ -37,7 +37,7 @@ STATICFILES_DIRS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_DIR, 'database.db'),  # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_DIR, 'data', 'database.db'),  # Or path to database file if using sqlite3.
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
